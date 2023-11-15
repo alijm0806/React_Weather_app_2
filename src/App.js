8b32f7f249f5ff6e89ecae7f0d85b5d5020c9e70
alijm0806 <alijm1104@gmail.com>
2023-11-14 20:12:11 -0600

    build inputs and temperature sections + styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import coldBg from './assets/cold.jpg'
+
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City ..." />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }

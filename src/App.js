4c1d2395a57a67c14418ab6dd976d00fbc08aa48
alijm0806 <alijm1104@gmail.com>
2023-11-17 16:46:37 -0600

    Make inputs dynamic

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Descriptions from './components/Descriptions'
 import { getFormattedWeatherData } from './assets/WeatherService';
 function App() {
 
+  const [city, setCity] = useState("Austin")
   const [weather, setWeather] = useState(null)
   const [units, setUnits] = useState("metric")
 
   useEffect(() => {
     const fetchWeatherData = async () => {
-      const data = await getFormattedWeatherData('paris', units);
+      const data = await getFormattedWeatherData(city, units);
       setWeather(data);
     };
     fetchWeatherData();
-  }, [units]);
+  }, [units, city]);
 
   const handleUnitsClick = (e) => {
     const button = e.currentTarget;
@@ -24,13 +25,20 @@ function App() {
     setUnits(isCelsius ? "metric" : "imperial");
   }
 
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  }
+
   return (
     <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
       <div className="overlay">
         {weather && (
           <div className="container">
             <div className="section section__inputs">
-              <input type="text" name="city" placeholder="Enter City ..." />
+              <input onKeyDown={enterKeyPressed} type="text" name="city" placeholder="Enter City ..." />
               <button onClick={(e) => handleUnitsClick(e)}>°F</button>
             </div>
             <div className="section section__temperature">

dd254f6af1c9cc7564d272bbb89fd671c91a51a2
alijm0806 <alijm1104@gmail.com>
2023-11-18 00:01:37 -0600

    Fix

--- src/components/Descriptions.js
+++ src/components/Descriptions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./descriptions.css";
+import "../components/descriptions.css";
 
 import { FaArrowUp, FaArrowDown, FaWind } from "react-icons/fa";
 import { BiHappy } from "react-icons/bi";
